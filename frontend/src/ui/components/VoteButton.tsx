49d7779f2314a9d62fd20ad45e4d37e58f397055
Roberto Salinas <robertosalinasaguilar99@gmail.com>
2024-08-24 12:36:01 -0600

    feat: removed unnecessary code

--- frontend/src/ui/components/VoteButton.tsx
+++ frontend/src/ui/components/VoteButton.tsx
@@ -1,31 +1,30 @@
 // components/VoteButton.tsx
-import Button from "@mui/material/Button";
-import constants from "../../config/constants";
-import React from "react";
+import Button from '@mui/material/Button'
+import React from 'react'
 
 interface VoteButtonProps {
-  disabled: boolean;
-  children: React.ReactNode;
+  disabled: boolean
+  children: React.ReactNode
 }
 
 const VoteButton: React.FC<VoteButtonProps> = ({ disabled, children }) => (
   <Button
-    variant="contained"
-    type="submit"
+    variant='contained'
+    type='submit'
     sx={{
-      height: "36px",
-      borderRadius: "8px",
-      maxWidth: "120px",
-      backgroundColor: "#15172A",
-      fontSize: "14px",
+      height: '36px',
+      borderRadius: '8px',
+      maxWidth: '120px',
+      backgroundColor: '#15172A',
+      fontSize: '14px',
       fontWeight: 700,
-      textTransform: "none",
-      width: "100%",
+      textTransform: 'none',
+      width: '100%'
     }}
     disabled={disabled}
   >
     {children}
   </Button>
-);
+)
 
-export default VoteButton;
+export default VoteButton
