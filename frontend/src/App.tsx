049a4a21aede92e15c1cf96b3887c30da7082c7b
Roberto Salinas <robertosalinasaguilar99@gmail.com>
2024-08-25 10:55:42 -0600

    refactor: Update imports and store setup in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import HomePage from "./ui/pages/HomePage/HomePage";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import theme from "./theme";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import React from 'react'
+import HomePage from './ui/pages/HomePage/HomePage'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import theme from './theme'
+import { Provider } from 'react-redux'
+import { setupStore } from './redux/store'
+
+const store = setupStore()
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
         <HomePage />
       </ThemeProvider>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
